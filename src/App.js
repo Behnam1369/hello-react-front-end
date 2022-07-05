b942896d376b7322fb18c98fafaffbea421b2bb1
behnam1369 <behnam.aghaali@yahoo.com>
2022-07-06 01:50:43 +0430

    Add greeting requirements

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Greeting from './components/Greeting';
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Greeting />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
